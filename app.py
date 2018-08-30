56b422750b02666f5c9369ce4093c8b434183e27
Jayden Choi <hohyenchoi@gmail.com>
2018-08-30 02:32:04 +0000

    echo bot

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+# json으로 바꾸기 위한 임포트
 import json
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,12 +11,32 @@ def hello():
     
 @app.route('/keyboard')
 def keyboard():
+    
+    # keyboard 딕셔너리 생성
     keyboard = {
       "type" : "buttons",
       "buttons" : ["메뉴", "로또", "고양이", "영화"]
     }
+    
+    # 딕셔너리를 json으로 바꿔서 리턴해주기 위한 코드
     json_keyboard = json.dumps(keyboard)
     return json_keyboard
     
+@app.route('/message',methods = ["post"])
+def message():
+    
+    # content라는 key의 value를 msg에 저장!
+    msg = request.json['content']
+    
+    json_return = {
+          "message" : {
+            "text" : msg
+        },
+          "keyboard" : {
+          "type" : "buttons",
+          "buttons" : ["메뉴", "로또", "고양이", "영화"]
+         }
+    }
+    return jsonify(json_return)
     
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
